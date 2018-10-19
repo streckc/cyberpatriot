6c0b93224c9f639f0e01e71f27504175b8a6bae5
Chad Streck <cstreck@tenable.com>
2018-10-19 08:27:24 -0500

    generalize index to use current readme as template

--- misc/build_topic_index.bash
+++ misc/build_topic_index.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-(
-	echo "# Topics of Discussion"
-	echo ""
-	echo "Many of these topics have been taken from blog articles or e-mails used during training."
-	echo ""
+TMP="README.tmp"
+OUT="README.md"
 
-	export COUNT=0
-	ls *.md | fgrep -v 'README.md' | sort -n | while read FILENAME; do
-		DESC=$(grep '^# ' $FILENAME | head -1 | sed -e 's/^# //g')
-		COUNT=$(($COUNT + 1))
-		echo "$COUNT. [$DESC](./$FILENAME)"
-	done
-) > README.md
+awk '!/^[0-9]{1,}\. \[/ { print } ' README.md > $TMP
+
+export COUNT=0
+ls *.md | fgrep -v 'README.md' | sort -n | while read FILENAME; do
+	DESC=$(grep '^# ' $FILENAME | head -1 | sed -e 's/^# //g')
+	COUNT=$(($COUNT + 1))
+	echo "$COUNT. [$DESC](./$FILENAME)" >> $TMP
+done
+
+mv $TMP $OUT
